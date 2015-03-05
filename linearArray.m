f3d30d2a534df550e5607bdf6c987660ed40d82b
RomandzeRoman <romanshanurin@avdfvsndfjnviofun.local>
2015-03-06 00:05:10 +0300

    Add polar plot

--- linearArray.m
+++ linearArray.m
@@ -13,14 +13,16 @@ stepByAlpha = 1;
 alphaP = alphaPDeg/180*pi;
 i = 0;
 for alphaDeg = -maxAlphaDeg : stepByAlpha : maxAlphaDeg
-    %alphaDeg
     i = i + 1;
     Y(i) = 0;
     alpha = alphaDeg/180*pi;
-    %for n = -N/2 : N/2 - 1
-    for n = 1 : N
+    for n = -N/2 : N/2 - 1
+    %for n = 1 : N
         Y(i) = Y(i) + A*exp(j*2*pi*n*d*(sin(alphaP)-sin(alpha))/lambda);
     end
 end
-plot([-maxAlphaDeg:stepByAlpha:maxAlphaDeg], real(Y))
+subplot(2,1,1);
+plot([-maxAlphaDeg:stepByAlpha:maxAlphaDeg], real(Y));
+subplot(2,1,2);
+polar([-maxAlphaDeg:stepByAlpha:maxAlphaDeg].*(pi/180), real(Y));
 grid on
