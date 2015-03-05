d6a7c79b58e1abc87d005d2139b03257deba5e23
RomandzeRoman <romanshanurin@avdfvsndfjnviofun.local>
2015-03-05 16:45:10 +0300

    Initial commit

---
+++ linearArray.m
@@ -0,0 +1,22 @@
+clear all
+%current angle of DN in degrees (from -maxAlphaDeg to maxAlphaDeg)
+alphaPDeg = 35; 
+%max angle of DN in degrees
+maxAlphaDeg = 90; 
+N = 50; % number of elements
+d = 2;% between elements
+lambda = 1/d; % length of wave
+omega = 2*pi*299792458/lambda;
+A = 1; % amplitude
+alphaP = alphaPDeg/180*pi;
+
+for alphaDeg = -maxAlphaDeg : maxAlphaDeg
+    index = alphaDeg+maxAlphaDeg+1;
+    Y(index) = 0;
+    alpha = alphaDeg/180*pi;
+    for n = 1 : N
+        Y(index) = Y(index) + A*exp(j*(2*pi*n*d*(sin(alphaP)-sin(alpha))));
+    end
+end
+plot([-maxAlphaDeg:maxAlphaDeg], abs(Y))
+grid on
